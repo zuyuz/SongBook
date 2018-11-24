ea59ad6170a1cfeced8afbe73389c1d21b3721cc
JuliaZvizlo <juliazvizlo@gmail.com>
2018-11-24 18:00:58 +0200

    Connect Firebase to the application

--- SongBook/SongBook/HomeViewController.swift
+++ SongBook/SongBook/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  SongBook
 //
 //  Created by JuliaZvizlo on 11/24/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
