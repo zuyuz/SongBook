66b05b2d598af0cd0a43523760f4c75f53c4b5f1
JuliaZvizlo <juliazvizlo@gmail.com>
2018-11-24 17:14:52 +0200

    Create empty xcode project

---
+++ SongBook/SongBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SongBook
+//
+//  Created by JuliaZvizlo on 11/24/18.
+//  Copyright © 2018 Mrrrnyaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
