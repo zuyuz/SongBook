33e046e09e22915f3e6a1397deabfa2c0e471e4f
Julia Zvizlo <juliazvizlo@gmail.com>
2019-05-14 10:27:24 +0300

    Create modules for testing the app.

---
+++ SongBook/SongBookUnitTests/SongListViewModelTests.swift
@@ -0,0 +1,23 @@
+//
+//  SongListViewModelTests.swift
+//  SongBookUnitTests
+//
+//  Created by Julia Zvizlo on 4/16/19.
+//  Copyright © 2019 Mrrrnyaaa. All rights reserved.
+//
+
+import XCTest
+@testable import SongBook
+
+class SongListViewModelTests: XCTestCase {
+
+    var viewModel: SongListViewModel?
+    
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}
